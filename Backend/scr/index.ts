7a2364d48469dfd94d8aca87047f67bad2f0aa2b
shivamprajapatii <shivamprajapati4794@gmail.com>
2025-01-10 08:34:48 +0530

    small changes in index.js

--- Backend/scr/index.ts
+++ Backend/scr/index.ts
@@ -42,50 +42,50 @@ app.post("/api/v1/signup", async (req, res) => {
 
 });
 
-app.post("/api/v1/signin", async (req, res) => {
-    const signinSchema = z.object({
-        username: z.string().min(3, "Username must be at least 3 characters long"),
-        password: z.string().min(5, "Password must be at least 8 characters long"),
-    });
-
-    const { username, password } = signinSchema.parse(req.body);
-
-    try {
-        const existingUser = await UserModel.findOne({
-            username: username,
+    app.post("/api/v1/signin", async (req, res) => {
+        const signinSchema = z.object({
+            username: z.string().min(3, "Username must be at least 3 characters long"),
+            password: z.string().min(5, "Password must be at least 8 characters long"),
         });
 
-        if (!existingUser) {
-            res.status(403).json({ message: "User not exist" });
-            return;
+        const { username, password } = signinSchema.parse(req.body);
+
+        try {
+            const existingUser = await UserModel.findOne({
+                username: username,
+            });
+
+            if (!existingUser) {
+                res.status(403).json({ message: "User not exist" });
+                return;
+            }
+
+            const passMatch = await bcrypt.compare(password, existingUser.password);
+
+            if (passMatch) {
+                const token = jwt.sign({
+                    id: existingUser._id,
+                }, JWT_SECRET);
+
+                res.status(200).json(token);
+
+            } else {
+                res.status(403).json({
+                    message: "incoorect credentials"
+                })
+            }
+
+        } catch (error) {
+            res.status(404).json({ message: "User not found" });
         }
 
-        const passMatch = await bcrypt.compare(password, existingUser.password);
-
-        if (passMatch) {
-            const token = jwt.sign({
-                id: existingUser._id,
-            }, JWT_SECRET);
-            res.status(200).json(token);
-
-        } else {
-            res.status(403).json({
-                message: "incoorect credentials"
-            })
-        }
-
-    } catch (error) {
-        res.status(404).json({ message: "User not found" });
-    }
-
-});
+    });
 
 
 app.post("/api/v1/content", userMiddleware, async (req, res) => {
     //@ts-ignore
-    console.log(req.userId);
     const { type, link,  title } = req.body;
-    console.log(req.body);
+    
     await ContenModel.create({
         type,
         link,
@@ -101,11 +101,36 @@ app.post("/api/v1/content", userMiddleware, async (req, res) => {
     
 });
 
-app.get("/api/v1/contents", (req, res) => {
+app.get("/api/v1/contents", userMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userId = req.userId;
 
+    try {
+        const contentData = await ContenModel.find({ userId }).populate("userId");
+        console.log(contentData);
+        if(!contentData){
+            res.send({message : "data not found"});
+        } else {
+            res.json(contentData);
+        }
+      
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-app.delete("/api/v1/content", (req, res) => {
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const { contentId } = req.body;
+    
+    await ContenModel.deleteMany({
+        contentId : contentId,
+        //@ts-ignore
+        userId : req.userId
+    });
+    res.json({message:"Data Deleted Successfully!"});
+});
+
+app.post("/api/v1/brain/share", (req,res) => {
 
 });
 
