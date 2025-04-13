a22e0ba994a76302f94b40c2703d75005dd8f9d1
shivamprajapatii <shivamprajapati4794@gmail.com>
2025-04-13 19:13:56 +0530

    changes in vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server : {
     port: 5173,
   },
-  base: './', // Ensure relative paths for assets
+  base: '/', // Ensure relative paths for assets
   resolve: {
     alias: {
       '@': '/src',
